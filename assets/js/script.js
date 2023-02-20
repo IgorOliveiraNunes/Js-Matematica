3960c4ca2f9ef12494e518e8e45133f1d961afec
Igor oliveira <igoron3008@gmail.com>
2023-02-20 15:01:37 -0300

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+let Valor1 = Math.round(1.2);
+console.log (Valor1);
+//Math.round
+//Volor sera arredondado tanto para baixo quanto para cima
+
+let Valor2 = Math.floor(2.9)
+console.log(Valor2)
+//Math.floor
+//Sempre sera arredondado para baixo, mesmo sendo 2.9
+
+let Valor3 = Math.ceil(2.1)
+console.log(Valor3)
+//Math.ceil
+//Sempre sera arredondado para cima, mesmo sendo 2.1
+
+let Valor4 = Math.min(6 , 30 ,40 ,10 , 4)
+console.log(Valor4)
+//Math.min
+//Retornara o menor valor
+
+let Valor5 = Math.max(4, 2, 1 , 5)
+console.log(Valor5)
+//Math.max
+//Retornara o maior valor
+
+let Valor6 = Math.random()
+console.log(Valor6)
+//Math.random
+//Retornarar um valor dentro de 0 a 1
+
+let Valor7 = Math.floor(Math.random() *100);
+console.log(Valor7)
+//Math.floor + Math.random
+//Math floor, para que não retornarar numero quebrado.
+//Math ramdon retornara um numero de 0 a 1
+// *100 para ser um numero real .
